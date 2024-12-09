4133d1a943d2a2e5909985bee9b7abfef79de7b6
Gabriel Vautour <gabriel.a.vautour@gmail.com>
2024-12-09 06:09:11 -0400

    Day 6: Part 2 progress

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -15,11 +15,11 @@ pub fn solve() -> Result<(), Box<dyn Error>> {
     let input = fetch_input(url, &session_token)?;
     //let input = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47";
 
-    let sum_part_1 = get_part_1(&input);
-    println!("Part 1: Total sum: {}", sum_part_1);
+    //let sum_part_1 = get_part_1(&input);
+    //println!("Part 1: Total sum: {}", sum_part_1);
 
-    //let sum_part_2 = get_part_2(&input);
-    //println!("Part 2: Total sum: {}", sum_part_2);
+    let sum_part_2 = get_part_2(&input);
+    println!("Part 2: Total sum: {}", sum_part_2);
 
     Ok(())
 }
@@ -97,88 +97,88 @@ fn get_part_1(input: &str) -> i32 {
     sum
 }
 
-// fn get_part_2(input: &str) -> i32 {
-//     let mut sum = 0;
+fn get_part_2(input: &str) -> i32 {
+    let mut sum = 0;
 
-//     // Destructure the two parts into `rules_input` and `updates_input`
-//     if let [rules_input, updates_input] = input.split("\n\n").collect::<Vec<&str>>()[..] {
-//         // Initialize an empty HashMap to store the data for rules
-//         let mut rules: HashMap<i32, Vec<i32>> = HashMap::new();
+    //     // Destructure the two parts into `rules_input` and `updates_input`
+    //     if let [rules_input, updates_input] = input.split("\n\n").collect::<Vec<&str>>()[..] {
+    //         // Initialize an empty HashMap to store the data for rules
+    //         let mut rules: HashMap<i32, Vec<i32>> = HashMap::new();
 
-//         // Parse the first part (rules_input) into the `rules` HashMap
-//         for line in rules_input.trim().lines() {
-//             // Split the line by '|', and directly parse the parts
-//             if let [key, value] = line.split('|').collect::<Vec<&str>>()[..] {
-//                 if let (Ok(key), Ok(value)) = (key.parse(), value.parse()) {
-//                     rules.entry(key).or_insert_with(Vec::new).push(value);
-//                 } else {
-//                     eprintln!("Error parsing numbers in pair: {}", line);
-//                 }
-//             }
-//         }
+    //         // Parse the first part (rules_input) into the `rules` HashMap
+    //         for line in rules_input.trim().lines() {
+    //             // Split the line by '|', and directly parse the parts
+    //             if let [key, value] = line.split('|').collect::<Vec<&str>>()[..] {
+    //                 if let (Ok(key), Ok(value)) = (key.parse(), value.parse()) {
+    //                     rules.entry(key).or_insert_with(Vec::new).push(value);
+    //                 } else {
+    //                     eprintln!("Error parsing numbers in pair: {}", line);
+    //                 }
+    //             }
+    //         }
 
-//         if DEBUG {
-//             // Print the map to see the result
-//             for (key, values) in &rules {
-//                 println!("{}: {:?}", key, values);
-//             }
-//         }
+    //         if DEBUG {
+    //             // Print the map to see the result
+    //             for (key, values) in &rules {
+    //                 println!("{}: {:?}", key, values);
+    //             }
+    //         }
 
-//         let indegree_table = build_indegree_table(&rules);
+    //         let indegree_table = build_indegree_table(&rules);
 
-//         // Parse the second part (updates_input) into a Vec<Vec<i32>> for pages
-//         let updates: Vec<Vec<i32>> = updates_input
-//             .lines()
-//             .map(|line| {
-//                 line.split(',')
-//                     .map(|x| x.trim().parse().unwrap())
-//                     .collect::<Vec<i32>>()
-//             })
-//             .collect();
+    //         // Parse the second part (updates_input) into a Vec<Vec<i32>> for pages
+    //         let updates: Vec<Vec<i32>> = updates_input
+    //             .lines()
+    //             .map(|line| {
+    //                 line.split(',')
+    //                     .map(|x| x.trim().parse().unwrap())
+    //                     .collect::<Vec<i32>>()
+    //             })
+    //             .collect();
 
-//         if DEBUG {
-//             println!("");
-//             // Print the updates to verify
-//             for update in &updates {
-//                 println!("{:?}", update);
-//             }
-//         }
+    //         if DEBUG {
+    //             println!("");
+    //             // Print the updates to verify
+    //             for update in &updates {
+    //                 println!("{:?}", update);
+    //             }
+    //         }
 
-//         for update in updates {
-//             let mut clone = update.clone();
+    //         for update in updates {
+    //             let mut clone = update.clone();
 
-//             while clone.len() > 1 {
-//                 let page = clone.pop().unwrap(); // Extract the last page element
-//                 let rule: Option<&Vec<i32>> = rules.get(&page);
+    //             while clone.len() > 1 {
+    //                 let page = clone.pop().unwrap(); // Extract the last page element
+    //                 let rule: Option<&Vec<i32>> = rules.get(&page);
 
-//                 if let Some(rule_vec) = rule {
-//                     if clone.iter().any(|&x| rule_vec.contains(&x)) {
-//                         sum += fix_order(update, &rules, &indegree_table);
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
+    //                 if let Some(rule_vec) = rule {
+    //                     if clone.iter().any(|&x| rule_vec.contains(&x)) {
+    //                         sum += fix_order(update, &rules, &indegree_table);
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //         }
 
-//         fn fix_order(
-//             update: Vec<i32>,
-//             rules: &HashMap<i32, Vec<i32>>,
-//             indegree_table: &HashMap<i32, usize>,
-//         ) -> i32 {
-//             println!("{:?}", update);
+    //         fn fix_order(
+    //             update: Vec<i32>,
+    //             rules: &HashMap<i32, Vec<i32>>,
+    //             indegree_table: &HashMap<i32, usize>,
+    //         ) -> i32 {
+    //             println!("{:?}", update);
 
-//             for (key, values) in rules {
-//                 println!("{}: {:?}", key, values);
-//             }
+    //             for (key, values) in rules {
+    //                 println!("{}: {:?}", key, values);
+    //             }
 
-//             0
-//         }
-//     } else {
-//         eprintln!("Input format is incorrect: expected two parts separated by a double newline.");
-//     }
+    //             0
+    //         }
+    //     } else {
+    //         eprintln!("Input format is incorrect: expected two parts separated by a double newline.");
+    //     }
 
-//     sum
-// }
+    sum
+}
 
 // fn build_indegree_table(rules: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
 //     let mut in_degree: HashMap<i32, usize> = HashMap::new();
--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -12,14 +12,15 @@ pub fn solve() -> Result<(), Box<dyn Error>> {
     let url = "https://adventofcode.com/2024/day/6/input";
     let session_token = env::var("SESSION_TOKEN").expect("SESSION_TOKEN must be set");
 
-    let input = fetch_input(url, &session_token)?;
+    //let input = fetch_input(url, &session_token)?;
+    let input: &str = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...";
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
-    let sum_part_1 = get_part_1(&grid);
-    println!("Part 1: Total sum: {}", sum_part_1);
+    //let sum_part_1 = get_part_1(&grid);
+    //println!("Part 1: Total sum: {}", sum_part_1);
 
-    //let sum_part_2 = get_part_2(&input);
-    //println!("Part 2: Total sum: {}", sum_part_2);
+    let sum_part_2 = get_part_2(&grid);
+    println!("Part 2: Total sum: {}", sum_part_2);
 
     Ok(())
 }
@@ -81,8 +82,86 @@ fn get_part_1(grid: &Vec<Vec<char>>) -> usize {
     visited.len()
 }
 
-// fn get_part_2(input: &str) -> i32 {
-//     let mut sum = 0;
+fn get_part_2(grid: &Vec<Vec<char>>) -> usize {
+    const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
-//     sum
-// }
+    fn turn(current_index: usize) -> usize {
+        (current_index + 1) % DIRECTIONS.len()
+    }
+
+    fn look(current_position: &(usize, usize), current_direction_index: usize) -> (i32, i32) {
+        let current_direction = DIRECTIONS[current_direction_index];
+        let new_destination = (
+            current_position.0 as i32 + current_direction.0,
+            current_position.1 as i32 + current_direction.1,
+        );
+
+        new_destination
+    }
+
+    let mut current_position: (usize, usize) = (0, 0);
+    let mut current_direction_index: usize = 0; // Start with 'Up' (index 0)
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+
+    // Find starting position
+    for row_index in 0..grid.len() {
+        for col_index in 0..grid[row_index].len() {
+            if grid[row_index][col_index] == '^' {
+                current_position = (row_index, col_index);
+                println!("Starting at: {:?}", current_position);
+                break;
+            }
+        }
+    }
+
+    // Find starting position
+    for row_index in 0..grid.len() {
+        for col_index in 0..grid[row_index].len() {
+            if grid[row_index][col_index] == '.' {
+                let object_position = (row_index, col_index);
+
+                loop {
+                    visited.insert(current_position);
+
+                    // look in front
+                    let (y, x) = look(&current_position, current_direction_index);
+
+                    // Out of bound (Up, Right, Down, Left)
+                    if y < 0 || x >= grid[0].len() as i32 || y >= grid.len() as i32 || x < 0 {
+                        break;
+                    }
+
+                    let y_usize: usize = y as usize;
+                    let x_usize: usize = x as usize;
+
+                    if object_position == (y_usize, x_usize) || grid[y_usize][x_usize] == '#' {
+                        current_direction_index = turn(current_direction_index);
+                    } else {
+                        // move to where you are looking
+                        current_position = (y_usize, x_usize);
+                    }
+                }
+            }
+        }
+    }
+
+    visited.len()
+}
+
+fn has_repeating_pattern(arr: &[i32]) -> bool {
+    let n = arr.len();
+
+    for pattern_length in 1..=n / 2 {
+        // Check for a repeating pattern of this length
+        for start in 0..=(n - 2 * pattern_length) {
+            let first = &arr[start..start + pattern_length];
+            let second = &arr[start + pattern_length..start + 2 * pattern_length];
+
+            if first == second {
+                return true; // Found a repeating pattern
+            }
+        }
+    }
+
+    false
+}
